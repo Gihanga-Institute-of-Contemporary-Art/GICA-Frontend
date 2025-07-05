9c96025a38cefcbce6086afc6eb39a326afe303b
Alvin Ashiatey <alvinashiatey@gmail.com>
2025-07-04 22:40:41 -0400

    dup index error

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,30 +1,18 @@
 // place// Utility functions
-export { 
-	formatTime12Hour, 
-	formatAllDateTimeRanges, 
-	getTimeRange, 
+export {
+	formatTime12Hour,
+	formatAllDateTimeRanges,
+	getTimeRange,
 	formatSingleDateTimeRange,
 	formatCompactDateTimeRange,
 	formatProgrammeDateFromArray,
 	getProgrammeStatusFromArray,
 	formatDateRange,
 	getPrimaryTime
-} from './utils';want to import through the `$lib` alias in this folder.
+} from './utils';
 export { default as VerticalSlider } from './components/VerticalSlider.svelte';
 export { default as Nav } from './components/Nav.svelte';
 export { default as DuotoneFilter } from './components/DuotoneFilter.svelte';
 
 // Stores
 export { navItems, initializeNavigation, updateNavItemTitle } from './stores/navStore';
-
-// Utility functions
-export {
-	formatTime12Hour,
-	formatAllDateTimeRanges,
-	getTimeRange,
-	formatSingleDateTimeRange,
-	formatProgrammeDateFromArray,
-	getProgrammeStatusFromArray,
-	formatDateRange,
-	getPrimaryTime
-} from './utils';
